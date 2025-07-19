50def1a6aa259efd65338e39c7e4128d13fe31ec
anoop-unq <padakantianoop7@gmail.com>
2025-07-19 15:26:20 +0530

    add vercel domain to provide access

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3800
-const allowedOrigins= ['http://localhost:5173','https://task-manager-mern-app-dj9e.onrender.com']
+const allowedOrigins= ['http://localhost:5173','https://task-manager-mern-app-navy.vercel.app']
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({origin:allowedOrigins,credentials:true}))
