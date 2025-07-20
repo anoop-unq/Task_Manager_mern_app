c3ca6ef9d77de65088ea3d10be7c0bc37fb70c82
anoop-unq <padakantianoop7@gmail.com>
2025-07-20 09:38:54 +0530

    Mahakalli

--- frontend/src/Lib/axios.js
+++ frontend/src/Lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3800/api/notes';
+const BASE_URL = 'https://task-manager-mern-app-dj9e.onrender.com/api/notes';
 
 export const api = axios.create({
   baseURL: BASE_URL,
